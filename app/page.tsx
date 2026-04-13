0a192e7276924225c576bec59e9554213554d200
nanda <tech03@ckpramuka.com>
2026-04-13 19:48:11 +0700

    refresh navbar

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
       {/* NAVBAR */}
       <nav className="fixed top-0 w-full z-50 backdrop-blur-xl bg-white/10 border-b border-white/10">
         <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
-          <h1 className="font-bold">Suciana</h1>
+          <h1 className="font-bold">Suciana Nur Arifandy</h1>
 
           <div className="hidden md:flex gap-6 items-center">
             <a href="#portfolio">Portfolio</a>
